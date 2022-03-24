83908f9230b722ca3aef0440a26172a85b50fff4
Eyüp Öztürk <97131091+ozturkeyupp54@users.noreply.github.com>
2022-03-24 19:09:04 +0300

    Create armstrong-query.py

---
+++ armstrong-query.py
@@ -0,0 +1,20 @@
+def armstrong_query():
+    el_numerico = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0"}
+    number = input("Please enter a number :")
+    number_set = set(number)
+    total = 0
+    len_num = len(number_set)
+    
+    if bool(number_set.difference(el_numerico)) == False:
+      list(number_set)
+      for j in number_set:
+       total = total + int(j)**int(len_num)
+    else:
+      print(" It is an invalid entry. Don't use non-numeric, float, or negative values!")
+      return
+    if total == int(number):
+      print("{} is an Armstrong number".format(number))
+    else:
+      print("{} is not an Armstrong number".format(number))
+
+armstrong_query()
